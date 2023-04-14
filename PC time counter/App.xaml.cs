973b6ad7d1bdd5437b0514b6aa7c9b0a8d85eb55
Vitalii <AnderHominem@users.noreply.github.com>
2023-04-14 22:52:48 +0300

    First Commit
    14.04.2023

---
+++ PC time counter/App.xaml.cs
@@ -0,0 +1,54 @@
+﻿using pc_time_counter;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Forms = System.Windows.Forms;
+
+namespace PC_time_counter_App
+{
+    public partial class App : Application
+    {
+
+        /*private readonly Forms.NotifyIcon notifyIcon;
+
+        public App()
+        {
+            notifyIcon = new Forms.NotifyIcon();
+        }
+        
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+
+            notifyIcon.Icon = new System.Drawing.Icon("Assets/Logo2.ico");
+            notifyIcon.Text = "Pc time counter";
+            notifyIcon.Click += NotifyIcon_Click;
+
+            notifyIcon.ContextMenu = new System.Windows.Forms.ContextMenu();
+            notifyIcon.ContextMenuStrip.Items.Add("Exit", Image.FromFile("Assets/Logo2.ico"));
+            notifyIcon.Visible = true;
+
+            base.OnStartup(e);
+        }
+
+
+        private void NotifyIcon_Click(object? sender, EventArgs e)
+        {
+            MainWindow.WindowState = WindowState.Normal;
+            MainWindow.Activate();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            notifyIcon.Dispose();
+            
+            base.OnExit(e);
+        }*/
+    }
+}
---
+++ PC time counter/MainWindow.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using PC_time_counter.Properties;
+
+namespace pc_time_counter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public int secondsCount = 0;
+        public int minutsCount = 0;
+        public int hoursCount = 0;
+        public int daysCount = 0;
+
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            DispatcherTimer disSeconds = new();
+            disSeconds.Tick += new EventHandler(DisSeconds_Tick);
+            disSeconds.Interval = new TimeSpan(0, 0, 0, 1);
+            disSeconds.Start();
+
+            Button reset = new();
+            reset.Click += new RoutedEventHandler(Reset_Click);
+
+            
+
+            GetSettings();
+        }
+
+        public void DisSeconds_Tick(object? sender, EventArgs e)
+        {
+            secondsCount++;
+
+            if (secondsCount == 60)
+            {
+                minutsCount++;
+                secondsCount = 0;
+                SaveSettings();
+            }
+
+            if (minutsCount == 60)
+            {
+                hoursCount++;
+                minutsCount = 0;
+            }
+
+            if (hoursCount == 24)
+            {
+                daysCount++;
+                hoursCount = 0;
+            }
+
+            if(dotsBlock.Visibility == Visibility.Visible)
+            {
+                dotsBlock.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                dotsBlock.Visibility = Visibility.Visible;
+            }
+
+            secondsBlock.Text = secondsCount.ToString("00");
+            minutsBlock.Text = minutsCount.ToString("00");
+            hoursBlock.Text = hoursCount.ToString("00");
+            daysBlock.Text = daysCount.ToString("00000");
+
+        }
+
+        public void SaveSettings()
+        {
+            Settings.Default.SaveMinutes = minutsCount;
+            Settings.Default.SaveHours = hoursCount;
+            Settings.Default.SaveDays = daysCount;
+            Settings.Default.Save();
+        }
+
+        public void GetSettings()
+        {
+            minutsCount = Settings.Default.SaveMinutes;
+            hoursCount = Settings.Default.SaveHours;
+            daysCount = Settings.Default.SaveDays;
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            string message = "Time was reseted";
+            MessageBoxResult result = MessageBox.Show("Do really want to reset your current time?", "Resetting time",
+                MessageBoxButton.OKCancel, MessageBoxImage.Warning);
+
+            if (result == MessageBoxResult.OK)
+            {
+                secondsCount = 0;
+                minutsCount = 0;
+                hoursCount = 0;
+                daysCount = 0;
+
+                Settings.Default.SaveMinutes = 0;
+                Settings.Default.SaveHours = 0;
+                Settings.Default.SaveDays = 0;
+                Settings.Default.Save();
+
+                MessageBox.Show(message);
+            }
+        }
+
+        private void TaskbarIcon_TrayLeftMouseDown(object? sender, RoutedEventArgs e)
+        {
+            this.Show();
+        }
+
+        private void Hide_Click(object? sender, RoutedEventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
+
